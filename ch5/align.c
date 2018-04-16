e6789b254292673123955e484068b0e07d66a39e
MissingPositron <galileoliu@foxmail.com>
2018-04-16 22:06:46 +0800

    chapter 5 struct

---
+++ ch5/align.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+struct test{
+    char ch;
+    short s;
+    int i;
+};
+
+int main(void){
+    struct test var;
+    printf("size of var : %d\n", sizeof(var));
+    return 0;
+}
---
+++ ch5/struct.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+//声明一个结构体变量
+struct test{
+    int array[5];
+    int a;
+    int *p;
+};
+
+int main(void){
+    struct test var;
+    int i;
+    for (i = 0; i < 5; i++)
+        printf("array [%d] : 0x%x\n", i + 1, (unsigned int)&(var.array[i]));
+    printf("a : 0x%x\n", (unsigned int)&(var.a));
+    printf("p : 0x%x\n", (unsigned int)&(var.p));
+    return 0;
+}
